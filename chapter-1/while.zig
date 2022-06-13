059c45edfab226adf20d4d062373a8e75bb1e08b
Norberto Martínez <norbux@live.com>
2022-06-13 00:36:46 -0500

    Added while with break

--- chapter-1/while.zig
+++ chapter-1/while.zig
@@ -34,3 +34,16 @@ test "while with continue" {
 
     try expect(sum == 4);
 }
+
+// While with break
+test "while with break" {
+    var sum: u8 = 0;
+    var i: u8 = 0;
+
+    while (i <= 3) : (i += 1) {
+        if (i == 2) break;
+        sum += i;
+    }
+
+    try expect(sum == 1);
+}
